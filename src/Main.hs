31840eab9ee8ed58c2f40ae4058919b55ee90d11
Yura Braiko <yurabraiko@gmail.com>
2017-01-16 00:59:56 +0200

    finish

--- src/Main.hs
+++ src/Main.hs
@@ -4,8 +4,13 @@ import Prelude
 
 import Methods
 
+import SpecialAlg
+
 a = 3.0
 b = 8.0
+
+-- a = -1.0
+-- b = 1.0
 fun x= sin (exp (x/3) + x)
 
 main :: IO ()
@@ -17,4 +22,12 @@ main = do
   putStrLn ("center rectangle :: \t\t"++(show$rectangleCenter a b (read iteerationCount :: Double) fun))
   putStrLn ("trapeze :: \t\t\t"++(show$trapeze a b (read iteerationCount :: Double) fun))
   putStrLn ("parabola :: \t\t\t"++(show$parabola a b (read iteerationCount :: Double) fun))
+  putStrLn ("newton :: \t\t\t"++(show$((newton a b 4 fun))))
+  putStrLn ("newton :: \t\t\t"++(show$newton a b 5 fun))
+  putStrLn ("hause :: \t\t\t"++(show$hause a b 4 fun))
+  putStrLn ("hause :: \t\t\t"++(show$hause a b 5 fun))
+  putStrLn ("hause :: \t\t\t"++(show$hause a b 6 fun))
+  putStrLn ("chebushov :: \t\t\t"++(show$chebushov a b 4 fun))
+  putStrLn ("chebushov :: \t\t\t"++(show$chebushov a b 5 fun))
+  putStrLn ("chebushov :: \t\t\t"++(show$chebushov a b 6 fun))
 
--- src/SpecialAlg.hs
+++ src/SpecialAlg.hs
@@ -3,27 +3,71 @@ module SpecialAlg where
 import CommonUtil
 
 data ChebushovData = ChebushovData {
-    xn :: [Double]
+    xn :: [Double],
     result :: Double,
-    n :: Integer
-    integtal :: Double -> Double
+    integtal :: Double -> Double,
+    value :: Double
   }
 
--- chebushov::Double -> Double -> Double -> (Double -> Double) -> Double
--- chebushov a b count fun =
---   ((b-a)/count)*sum1
---   where
---     sum1 = result (while (\ itState -> ((n itState )<=0 ,
---         itState {
---
---           })))
+newton :: Double -> Double -> Double -> (Double -> Double) -> Double
+newton a b count fun = ((b - a)/2) * integralValue
+  where
+    operand1 = (b+a)/2
+    operand2 = (b-a)/2
+    h = ((b-a)/count)
+    integralValue = result ( while (\itState -> (xn itState == [], itState {
+        xn = tail$xn$itState,
+        result = result itState + (head$xn$itState)*(integtal itState (value itState)),
+        value = value itState + h
+      })) ChebushovData {
+        xn = if count == 4 then
+            [7.0/45.0,32.0/45.0,12.0/45.0,32.0/45.0,7.0/45.0]
+          else
+            [19.0/144.0,75.0/144.0,50.0/144.0,50.0/144.0,75.0/144.0,19.0/144.0],
+        result =0,
+        value = a,
+        integtal = fun})
+
+
+hause :: Double -> Double -> Double -> (Double -> Double) -> Double
+hause a b count fun = ((b - a)/2) * integralValue
+  where
+    operand1 = (b+a)/2
+    operand2 = (b-a)/2
+    h = ((b-a)/count)*2
+    integralValue = result ( while (\itState -> (xn itState == [], itState {
+        xn = tail$xn$itState,
+        result = result itState + (head$xn$itState)*(integtal itState (value itState))
+      })) ChebushovData {
+        xn =
+          (case count of
+            4 ->  [7.0/45.0,32.0/45.0,12.0/45.0,32.0/45.0,7.0/45.0]
+            5 ->  [7.0/45.0,32.0/45.0,12.0/45.0,32.0/45.0,7.0/45.0]
+            _ ->  [7.0/45.0,32.0/45.0,12.0/45.0,32.0/45.0,7.0/45.0]
+        ),
+        result =0,
+        value = operand1,
+        integtal = fun})
+
+
+chebushov :: Double -> Double -> Double -> (Double -> Double) -> Double
+chebushov a b count fun = ((b - a)/count) * integralValue
+  where
+    operand1 = (b+a)/2
+    operand2 = (b-a)/2
+    nodes =(case count of
+                4 ->  [-0.794655,-0.187593,0.187593,0.794655]
+                5 ->  [-0.832498,-0.374541,0.0,0.374541,0.832498]
+                _ ->  [-0.866257,-0.422519,-0.266636,0.266636,0.422519,0.866257]
+            )
+    integralValue = result ( while (\itState -> (xn itState == [], itState {
+        result = (result itState) + (integtal itState (operand1 + operand2 *(head (xn itState)))),
+        xn = tail$xn$itState
+      })) ChebushovData {
+        xn = nodes,
+        result =0,
+        value = operand1 ,
+        integtal = fun})
 
 
 
-
---
---       sum1 = result (while (\ itState -> ( (value itState)  + (step itState ) < (maxV itState ),
---                   itState {
---                     value = (value itState) + (step itState )*2,
---                     result = ((result itState) + ((integtal itState ( (value itState) + (step itState )*2))))}))
---                   RectangleIteration {value = a-h, result = 0, Methods.maxV = b, step = h, integtal = fun})
